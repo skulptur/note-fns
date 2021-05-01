50a6ac6fc9db9fff615bdeafb1e9424d3dfcdb09
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-01 15:27:23 -0300

    re export all from music-fns

---
+++ src/lib/musicFns.ts
@@ -0,0 +1,133 @@
+// @ts-ignore
+import {
+  Chord as internalChord,
+  Interval as internalInterval,
+  Mode as internalMode,
+  Scale as internalScale,
+  NOTES as internalNOTES,
+  accidentalToLetter as internalAccidentalToLetter,
+  accidentalToSymbol as internalAccidentalToSymbol,
+  areEqual as internalAreEqual,
+  createChord as internalCreateChord,
+  createMelody as internalCreateMelody,
+  createScale as internalCreateScale,
+  flatToSharp as internalFlatToSharp,
+  getAccidental as internalGetAccidental,
+  getChromaticCPosition as internalGetChromaticCPosition,
+  getDominant as internalGetDominant,
+  getIntervals as internalGetIntervals,
+  getLeadingTone as internalGetLeadingTone,
+  getMediant as internalGetMediant,
+  getNote as internalGetNote,
+  getOctave as internalGetOctave,
+  getRoot as internalGetRoot,
+  getNoteOnDegree as internalGetNoteOnDegree,
+  getSubdominant as internalGetSubdominant,
+  getSubmediant as internalGetSubmediant,
+  getSupertonic as internalGetSupertonic,
+  getTonic as internalGetTonic,
+  hasAccidental as internalHasAccidental,
+  hasAccidentalLetter as internalHasAccidentalLetter,
+  hasAccidentalSymbol as internalHasAccidentalSymbol,
+  hasOctave as internalHasOctave,
+  haveSameOctave as internalHaveSameOctave,
+  isAnhemitonic as internalIsAnhemitonic,
+  isAscending as internalIsAscending,
+  isCohemitonic as internalIsCohemitonic,
+  isDescending as internalIsDescending,
+  isDiatonic as internalIsDiatonic,
+  isFifth as internalIsFifth,
+  isFlat as internalIsFlat,
+  isHemitonic as internalIsHemitonic,
+  isHeptatonic as internalIsHeptatonic,
+  isHexatonic as internalIsHexatonic,
+  areIntervals as internalAreIntervals,
+  isMode as internalIsMode,
+  isNatural as internalIsNatural,
+  isOctatonic as internalIsOctatonic,
+  isOctave as internalIsOctave,
+  isPentatonic as internalIsPentatonic,
+  isScale as internalIsScale,
+  hasIntervalAmount as internalHasIntervalAmount,
+  isNote as internalIsNote,
+  areNotes as internalAreNotes,
+  isSemitone as internalIsSemitone,
+  isSharp as internalIsSharp,
+  isTone as internalIsTone,
+  isTriad as internalIsTriad,
+  normalize as internalNormalize,
+  noteToFrequency as internalNoteToFrequency,
+  noteToMidi as internalNoteToMidi,
+  noteToObject as internalNoteToObject,
+  objectToNote as internalObjectToNote,
+  sharpToFlat as internalSharpToFlat,
+  transferAccidental as internalTransferAccidental,
+  transferStyle as internalTransferStyle,
+  transpose as internalTranspose,
+  // @ts-ignore
+} from 'music-fns';
+
+export const Chord = internalChord;
+export const Interval = internalInterval;
+export const Mode = internalMode;
+export const Scale = internalScale;
+export const NOTES = internalNOTES;
+export const accidentalToLetter = internalAccidentalToLetter;
+export const accidentalToSymbol = internalAccidentalToSymbol;
+export const areEqual = internalAreEqual;
+export const createChord = internalCreateChord;
+export const createMelody = internalCreateMelody;
+export const createScale = internalCreateScale;
+export const flatToSharp = internalFlatToSharp;
+export const getAccidental = internalGetAccidental;
+export const getChromaticCPosition = internalGetChromaticCPosition;
+export const getDominant = internalGetDominant;
+export const getIntervals = internalGetIntervals;
+export const getLeadingTone = internalGetLeadingTone;
+export const getMediant = internalGetMediant;
+export const getNote = internalGetNote;
+export const getOctave = internalGetOctave;
+export const getRoot = internalGetRoot;
+export const getNoteOnDegree = internalGetNoteOnDegree;
+export const getSubdominant = internalGetSubdominant;
+export const getSubmediant = internalGetSubmediant;
+export const getSupertonic = internalGetSupertonic;
+export const getTonic = internalGetTonic;
+export const hasAccidental = internalHasAccidental;
+export const hasAccidentalLetter = internalHasAccidentalLetter;
+export const hasAccidentalSymbol = internalHasAccidentalSymbol;
+export const hasOctave = internalHasOctave;
+export const haveSameOctave = internalHaveSameOctave;
+export const isAnhemitonic = internalIsAnhemitonic;
+export const isAscending = internalIsAscending;
+export const isCohemitonic = internalIsCohemitonic;
+export const isDescending = internalIsDescending;
+export const isDiatonic = internalIsDiatonic;
+export const isFifth = internalIsFifth;
+export const isFlat = internalIsFlat;
+export const isHemitonic = internalIsHemitonic;
+export const isHeptatonic = internalIsHeptatonic;
+export const isHexatonic = internalIsHexatonic;
+export const areIntervals = internalAreIntervals;
+export const isMode = internalIsMode;
+export const isNatural = internalIsNatural;
+export const isOctatonic = internalIsOctatonic;
+export const isOctave = internalIsOctave;
+export const isPentatonic = internalIsPentatonic;
+export const isScale = internalIsScale;
+export const hasIntervalAmount = internalHasIntervalAmount;
+export const isNote = internalIsNote;
+export const areNotes = internalAreNotes;
+export const isSemitone = internalIsSemitone;
+export const isSharp = internalIsSharp;
+export const isTone = internalIsTone;
+export const isTriad = internalIsTriad;
+export const normalize = internalNormalize;
+export const noteToFrequency = internalNoteToFrequency;
+export const noteToMidi = internalNoteToMidi;
+export const noteToObject = internalNoteToObject;
+export const objectToNote = internalObjectToNote;
+export const sharpToFlat = internalSharpToFlat;
+export const transferAccidental = internalTransferAccidental;
+export const transferStyle = internalTransferStyle;
+export const transpose = internalTranspose;
